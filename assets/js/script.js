c7cc509754a5214bb34a7e4e2fe9d3de76f2c86d
dejagraver <dejaamaartin@gmail.com>
2021-03-14 11:45:02 -0400

    finalPassword generated

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,29 +12,29 @@
 //Character types prompt for numeric
 //Character types prompt for special characters 
 //Generate Password 
-
-
 //Create Arrays alongside possibilies IF criteria asked for isnt selected. 
+
+
 var generateBtn = document.querySelector("#generate");
 
 var numericCharacters = ["0","1", "2", "3", "4", "5", "6", "7", "8", "9"]
 var specialCharacters = [",", "?", ".", ">", "<", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "`", "{", "}", "[", "]", "|", "œ", "∑", "´", "®", "†", "¥", "¨", "ˆ", "ø", "π", "“", "‘", "«", "å", "ß", "∂", "ƒ", "©", "˙", "˙", "∆", "˚", "˚", "¬", "…", "æ", "÷", "≥", "≤", "µ", "˜", "∫", "√", "ç", "≈" ]
 var upperCaseCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 var lowerCaseCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-
+var passwordLength;
 function choosePasswordOptions() {
     criteriaPrompt = confirm("Welcome to the Password Generator! You have the option of including Numeric characters, Special Characters, Uppercase letters, and Lowercase letters. You must choose a mininmum of one character type. You can only choose a password length between 8 and 128! ")
- var passwordLength = parseInt(prompt("How many charcters long do you want?"));
+    passwordLength = parseInt(prompt("How many charcters long do you want?"));
     if (isNaN(passwordLength)) {
         window.alert ("Password Length must be between 8 and 128!");
         return;
     }
     if (passwordLength < 8)  {
-        alert("Password must be atleast 8 characters")
+        alert("Password must be atleast 8 characters");
         return;
     }
      if (passwordLength > 128)  {
-        alert("Password must be less then 129 characters")
+        alert("Password must be less then 129 characters");
         return;
     }
     // invoke new prompt between conditions
@@ -72,14 +72,11 @@ function choosePasswordOptions() {
         if (wantsNumericCharacters === false && wantsSpecialCharacters === false && wantsUpperCaseCharacters === false && wantsLowerCaseCharacters === false) {
         alert("Error, Please select a minimum of one Character Type.")
         }
-        
-
-
     //Password Options 
     var passwordOptions = {
         length: passwordLength, 
         numbers: wantsNumericCharacters,
-        specialCharacters: wantsSpecialCharacters,
+        special: wantsSpecialCharacters,
         uppercase: wantsUpperCaseCharacters,
         lowercase: wantsLowerCaseCharacters,  
     }
@@ -91,25 +88,38 @@ function choosePasswordOptions() {
 function getRandom(arr) {
     var randomIndex = Math.floor(Math.random() * arr.length);
     var randomElement = arr[randomIndex];
+    console.log(randomElement)
     return randomElement;
 }
 
+//take items of an array and make them into a string 
+//need to randomize the array and need to tell it how long it should be through the use of var passwordLength 
+// need a for loop with i<options.length 
+
 function generatePassword() {
+    var finalPasswordoptions = []
     var finalPassword = []
+    characterElements =  lowerCaseCharacters.join("")
     var options = choosePasswordOptions(); 
     console.log(options) 
-         if (options.numbers) {
-            finalPassword.push(...numericCharacters)
-        } if (options.specialCharacters) {
-            finalPassword.push(...specialCharacters)
-        } if (options.uppercase) {
-            finalPassword.push(...upperCaseCharacters)
-        } if (options.lowercase) {
-            finalPassword.push(...lowerCaseCharacters)
+    // for (var i = 0; i <options.length; i++) { getranodm from the list of selceted character as seen in the IF loops 
+        if  (options.numbers===true) {
+            finalPasswordoptions.push(...numericCharacters);
+        } if (options.special===true) {
+            finalPasswordoptions.push(...specialCharacters);
+        } if (options.uppercase===true) {
+            finalPasswordoptions.push(...upperCaseCharacters);
+        } if (options.lowercase===true) {
+            finalPasswordoptions.push(...lowerCaseCharacters);
+        } 
+        for (var i = 0; i < options.length; i++) {
+            var finalCharacter = getRandom(finalPasswordoptions)
+            finalPassword.push(finalCharacter)
         }
-    var randomElement = getRandom(finalPassword)
-    //youre declaring the varible but not using it anywhere 
-    return finalPassword}
+    //var randomElement = getRandom(finalPassword)
+    //final password needs to be a string 
+    return finalPassword.join("")
+}
     
 
 
